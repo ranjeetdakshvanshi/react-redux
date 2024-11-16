1bbc7ee1c7e09030374c6ff111d9df2a1895beaa
|| 𝐑𝐀𝐍𝐉𝐄𝐄𝐓 𝐏𝐑𝐀𝐉𝐀𝐏𝐀𝐓𝐈 || <164492985+ranjeetdakshvanshi@users.noreply.github.com>
2024-11-16 12:52:20 +0000

    texted some example

--- React-app/src/demo.jsx
+++ React-app/src/demo.jsx
@@ -1,4 +1,10 @@
 export default function Demo()
-{
-    return <button>Demo</button>
+{   let name="Ranjeet";
+    let Cname="Nothing"
+    let cityname=function city(){
+        return India;
+    }
+    return  <h4>Hey Guys, My name {name} and Caname {Cname}I'm from {cityname}</h4>
+  
+
 }
